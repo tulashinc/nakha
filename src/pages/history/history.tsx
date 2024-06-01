ce287f78c45fb3396eacf210087f1c64c2eb53c4
Tulashi <tulashi58@gmail.com>
2024-06-01 16:49:33 -0400

    designed contain page with logo.

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -1,5 +1,62 @@
+import './history.css'
+
 const History = () => {
-	return <div>History of Newah Nakha New York</div>
+	return (
+		<div>
+			<p className="main-heading">History of Newah Nakha New York</p>
+			<p className="sub-heading">Welcome to Newah Nakha</p>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam vitae
+				saepe, possimus dignissimos repudiandae maiores ratione molestias fugiat
+				ea architecto quasi in voluptatum porro beatae, distinctio quia non
+				natus velit. Dignissimos vero ipsum non dolorem nobis et mollitia
+				voluptatibus animi? Quos, velit? Dolorem officiis doloribus magni,
+				recusandae ab, modi necessitatibus facere consequuntur obcaecati aperiam
+				laborum, magnam aut vero quae mollitia? Dignissimos nihil voluptatum
+				iure fugit, numquam qui! Et voluptatem quia ea nisi odit, eum animi
+				velit unde ad libero totam facere, ut, earum consequatur. Tempora nihil
+				dignissimos natus quae eligendi! Hic, soluta quaerat vel modi tempore
+				voluptatem, maxime nam fugit voluptate quod exercitationem. Ea eos
+				officia, molestiae provident minima ipsa excepturi, aspernatur nemo cum
+				saepe eum blanditiis ipsam fugiat nobis! Dolorum at laboriosam, vero
+				earum eos ex expedita consequatur, ut quod hic debitis placeat modi.
+				Provident autem, dolorum voluptatum, placeat eveniet est nam quisquam,
+				quo maiores mollitia dolorem nemo doloremque? Ratione consectetur ipsa
+				distinctio ipsam asperiores similique aspernatur commodi, numquam enim.
+				Perferendis corporis enim debitis illum quidem omnis laborum at error
+				veniam ab? Consequuntur dolor debitis ab dicta possimus aspernatur?
+				Reprehenderit placeat, velit explicabo corrupti pariatur eveniet?
+				Ratione dolorum illo voluptatum nostrum, assumenda deleniti, numquam ad
+				id perspiciatis sapiente obcaecati nisi cupiditate? Aliquid vel dicta
+				facere impedit soluta quas corrupti! Dolor possimus quas iusto
+				cupiditate ea est voluptates recusandae deleniti rerum non, itaque
+				assumenda voluptas enim, explicabo quia necessitatibus sapiente, aut
+				eligendi illo? Molestias accusantium cumque natus facilis necessitatibus
+				voluptate. Nisi dolores qui quam odit omnis ad quibusdam dolorem? Ad
+				veritatis minima velit? Culpa, quas. Omnis, eveniet ratione illo,
+				reiciendis pariatur veniam perferendis eligendi delectus porro facilis
+				dolor. Repellat, alias! Itaque commodi placeat repudiandae saepe facere
+				fuga odit, aliquam id libero cumque eligendi illum ullam ducimus esse?
+				Libero porro, ipsum animi ut alias rem necessitatibus placeat in impedit
+				tempore iure? Ullam, possimus. Reiciendis impedit ab totam, vero
+				quibusdam ex architecto molestias recusandae aut animi excepturi sequi
+				doloribus repudiandae, vitae nulla, natus ad. Facere ab quia, quisquam
+				alias a enim repellat. Enim, natus! Laboriosam incidunt deserunt
+				inventore cupiditate, mollitia modi placeat, illo sunt aliquid amet
+				nesciunt quibusdam libero molestiae pariatur rem! Consectetur debitis a
+				sequi aspernatur illum pariatur eius nihil accusantium. Fugiat ut qui
+				assumenda voluptate exercitationem laudantium quaerat dolorem explicabo
+				aliquid. Nulla, temporibus eligendi. Debitis velit autem ea consequatur
+				quia labore quae provident modi, at aliquid beatae obcaecati maxime.
+				Iste? Mollitia qui aut placeat repellat temporibus, suscipit quas quos
+				voluptates laboriosam sed quae magni dolor distinctio maxime tempora
+				sapiente! Voluptatem, accusamus minus! Dolorem sunt fugit facere at ex
+				corrupti quis. Eos dolor non repellat quasi quis? Distinctio, commodi
+				voluptatibus, quisquam libero itaque repudiandae facilis amet recusandae
+				iusto, obcaecati voluptates.
+			</p>
+		</div>
+	)
 }
 
 export default History
