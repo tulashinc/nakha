c6add92029dbe0a71c87b5f29893d1ed51cb2049
Tulashi <tulashi58@gmail.com>
2024-06-09 20:18:07 -0400

    added menubar animation

--- src/pages/advertisements/advertisements.tsx
+++ src/pages/advertisements/advertisements.tsx
@@ -3,9 +3,9 @@ import './advertisements.css'
 const Advertisements = () => {
 	return (
 		<div className="adv-container">
-			<div className="advertises">List of Events</div>
-			<div className="advertises">Advertisement 1</div>
-			<div className="advertises">Advertisement 2</div>
+			<div className="advertises">Upcoming List of Events</div>
+			<div className="advertises">Your Business Card is here</div>
+			<div className="advertises">Publish your business with us</div>
 			<div className="advertises">Advertisement 3</div>
 		</div>
 	)
