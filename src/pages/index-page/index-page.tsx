bfbc305659068b62375f1419894c3e5b6e9b35d1
Tulashi <tulashi58@gmail.com>
2024-06-17 23:10:06 -0400

    menu and sub menu.

--- src/pages/index-page/index-page.tsx
+++ src/pages/index-page/index-page.tsx
@@ -3,12 +3,12 @@ const IndexPage = () => {
 		<div>
 			<p className="main-heading">This is INDEX page.</p>
 			<p className="sub-heading">Welcome to Newah Nakha New York</p>
-			<span>
+			<p>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
 				explicabo doloremque. Mollitia laboriosam suscipit molestiae! Dolore
 				temporibus at cumque quo, veniam perferendis. Aliquam ut, consectetur
 				delectus quisquam libero dolorem neque?
-			</span>
+			</p>
 		</div>
 	)
 }
